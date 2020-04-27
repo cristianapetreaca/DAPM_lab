b6fdbea2ab10b505cb33d45637d70540cf30ddc3
Cristiana Petreaca <cristianapetreaca@gmail.com>
2020-04-27 19:25:26 +0300

    lab 10

---
+++ photo-gallery_1/src/app/services/photo.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import {
+  Plugins,
+  CameraResultType,
+  Capacitor,
+  FilesystemDirectory,
+  CameraPhoto,
+  CameraSource,
+} from "@capacitor/core";
+const { Camera, Filesystem, Storage } = Plugins;
+
+@Injectable({
+  providedIn: "root",
+})
+export class PhotoService {
+  constructor() { }
+
+  public photos: Photo[] = [];
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    this.photos.unshift({
+      filepath: "",
+      webviewPath: capturedPhoto.webPath,
+    });
+  }
+}
+
+interface Photo {
+  filepath: string;
+  webviewPath: string;
+  base64?: string;
+}
